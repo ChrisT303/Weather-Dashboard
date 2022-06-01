9ad4dff90c0eb2cc75f8546f01d0ef8ebbf68b51
deftonechris <deftonechris@msn.com>
2022-06-01 15:08:01 -0600

    cleaned up JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,29 @@
-let citiesArr = [];
+let citiesArr = eval(localStorage.cities) || [];
+saveStorage();
+
+$('#search-button').on('click', handleWeather);
+
+$("#city-input").keypress(function (event) {
+  if (event.key === "Enter") {
+    handleWeather();
+  }
+});
+
+function saveStorage(city) {
+  if (city){
+    if (!citiesArr.includes(city)) citiesArr.push(city[0].toUpperCase()+city.substr(1));
+  }
+  $('#searchHistory').html('');
+  citiesArr.forEach(city => {
+    $('#searchHistory').append(`<button class="btn-primary" onclick="handleHistory('${city}')">${city}</button>`)
+  });
+  localStorage.cities = JSON.stringify(citiesArr);
+};
+
+function handleHistory(city) {
+  $('input').val(city);
+  handleWeather();
+}
 
 function handleWeather() {
   let city = document.querySelector('input').value;
@@ -9,35 +34,33 @@ function handleWeather() {
 
     let url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
-    
+
     fetch(url2).then(data => data.json()).then(data => {
+      saveStorage(city);
       console.log(data);
       let icon = `http://openweathermap.org/img/w/${data.current.weather[0].icon}.png`
 
-      let main = data.current.weather[0].main;
       let description = data.current.weather[0].description;
       let temp = Math.round(data.current.temp);
       let humidity = data.current.humidity;
       let windSpeed = Math.round(data.current.wind_speed);
       let uvi = data.current.uvi;
       let name = city;
-      
 
-      document.getElementById('icon').setAttribute('src',icon);
+      document.getElementById('icon').setAttribute('src', icon);
       document.getElementById("cityDescription").innerHTML = description;
       document.getElementById("cityTemp").innerHTML = temp + "°";
       document.getElementById("cityHumididty").innerHTML = humidity + "%";
       document.getElementById("cityWindSpeed").innerHTML = windSpeed + " mph";
       document.getElementById("cityUVI").innerHTML = uvi;
       document.getElementById("cityName").innerHTML = name;
-      document.getElementById("currentDate").innerHTML = moment().format("MMM Do YYYY"); 
- 
+      document.getElementById("currentDate").innerHTML = moment().format("MMM Do YYYY");
 
-      $("#cityUVI").each(function() {
+      $("#cityUVI").each(function () {
         if (uvi < 3) {
           $(this).addClass("favorable");
         }
-        else if(uvi >= 3 && uvi <= 8) {
+        else if (uvi >= 3 && uvi <= 8) {
           $(this).addClass("moderate");
         }
         else {
@@ -48,89 +71,33 @@ function handleWeather() {
       document.getElementById('forecast').innerHTML = '';
 
       for (let i = 0; i < 5; i++) {
-        let date =  moment().add(i+1,"days").format("MM/DD/YYYY"); 
+        let date = moment().add(i + 1, "days").format("MM/DD/YYYY");
         let icon2 = data.daily[i].weather[0].icon;
         let temp = data.daily[i].temp.day;
         let wind = data.daily[i].wind_speed;
         let humidity = data.daily[i].humidity;
 
-        document.getElementById('forecast').innerHTML += 
-        `<div class="forecastCard">
+        document.getElementById('forecast').innerHTML +=
+          `<div class="forecastCard">
               <h5>${date}</h5>
               <img src="http://openweathermap.org/img/w/${icon2}.png">
               <h6>Temp: ${Math.round(temp) + "°"}</h6>
               <h6>Wind: ${Math.round(wind) + " mph"}</h6>
              <h6>Humidity: ${humidity + "%"}</h6>
               </div>`
-             
-      }
-      
-      for (let i = 0; i < citiesArr.length; i++) {    
-       if(citiesArr[i] == city){
-            return;
-         } 
-        
-      }
-        citiesArr.unshift(city);
 
-        handleCities();
-        pastSearhces();
+      }
 
-        localStorage.setItem("searchHistory", JSON.stringify(citiesArr));
+      document.querySelector(".hide").style.display = "block";
     });
-    document.querySelector(".hide").style.display = "block";
-    
-    pastSearhces();
-   
   });
-}
-
-$("#city-input").keypress(function (event) {
-    if (event.key === "Enter") {
-    $("#search-button").click();
-  }
-});
+};
 
 
+// $(window).on("load", function () {
+//   $("#city-input").prop("value", citiesArr[0]);
+//   handleWeather();
+// })
 
-function handleCities () {
-  $("#searchHistory").empty();
 
-  for (let i = 0; i < citiesArr.length; i++) {
-    
-    let cityBtn = $('<button>');
-    cityBtn.addClass("city btn btn-primary");
-    cityBtn.attr("city-input", citiesArr[i]);
-    cityBtn.text(citiesArr[i]);
-    $("#searchHistory").append(cityBtn);
-    
-  }
-  
-}
-
-function pastSearhces() {
-   $(".btn-primary").on("click", function () {
-     let pastCity = $(this).attr("city-input");
-     $("#city-input").prop("value", pastCity);
-     handleWeather();
-
-   });
-  }
-
-function displayCities() {
-  let storedCities = JSON.parse(localStorage.getItem("searchHistory"));
-
-  if (storedCities !== null) {
-    citiesArr = storedCities;
-  }
-   handleCities(citiesArr);
-}
-
-$(window).on("load", function(){
-  $("#city-input").prop("value", citiesArr[0]);
-  handleWeather();
-})
-
-pastSearhces();
-displayCities();
 
