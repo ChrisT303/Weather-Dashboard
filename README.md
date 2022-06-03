0f4ab40a4f2df4016459d6414840d6413c1afe1b
deftonechris <deftonechris@msn.com>
2022-06-02 20:02:57 -0600

    added comments, README, and added to style

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Weather-Dashboard
+This app was created for the user to search the weather anywhere in the world.
+
+The app displays the current weather conditons for searches city along with:
+
+-The current date and year
+-Tempature in degrees Farenheit
+-The current humidity
+-Current wind speed
+-UVI index with 3 colors indicating:
+   -Favorable: green 
+   -Moderate: purple
+   -Severe: red
+-The 5 day forecast is diplayed below the main weather card
+
+
+To the side of the current weather conditons is a search bar for the user to input the city of thier choice. The city saves to local storage and displays as a button to go back to the cities weather upon click or enter key press. 
+
+The user can clear the search history at any time and it will relaod to the start with all items cleared for a new search.
+
+Javascript
+jquery
+bootstrap
+CSS
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,41 @@
+// evalutates local storage or empty array
 let citiesArr = eval(localStorage.cities) || [];
+// calls save storage function
 saveStorage();
 
+// click lisener for searhbutton then runs weather function
 $('#search-button').on('click', handleWeather);
-
+// passes in city searched and enables the user to press enter to search 
 $("#city-input").keypress(function (event) {
   if (event.key === "Enter") {
     handleWeather();
   }
 });
-
+// Save to local storage function 
 function saveStorage(city) {
+  // pushes city to button if saved in local storage and captalizes first letter
   if (city){
-    if (!citiesArr.includes(city)) citiesArr.push(city[0].toUpperCase()+city.substr(1));
-  }
+    if (!citiesArr.includes(city))citiesArr.push(city[0].toUpperCase()+city.substr(1));
+  } 
+  // creates each button in html
   $('#searchHistory').html('');
   citiesArr.forEach(city => {
     $('#searchHistory').append(`<button class="btn-primary" onclick="handleHistory('${city}')">${city}</button>`)
   });
   localStorage.cities = JSON.stringify(citiesArr);
 };
-
+// handles search hisotry input
 function handleHistory(city) {
   $('input').val(city);
   handleWeather();
 }
-
+// main function to pull API
 function handleWeather() {
   let city = document.querySelector('input').value;
+  // if no city entered nothing happens 
   if (!city) return;
   let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+  // API fetch
   fetch(url).then(data => data.json()).then(data => {
     let { lat, lon } = data[0];
 
@@ -40,7 +47,7 @@ function handleWeather() {
       console.log(data);
       let icon = `http://openweathermap.org/img/w/${data.current.weather[0].icon}.png`
       
-
+      // varibales for each weather retlated attribute 
       let description = data.current.weather[0].description;
       let temp = Math.round(data.current.temp);
       let humidity = data.current.humidity;
@@ -48,7 +55,7 @@ function handleWeather() {
       let uvi = data.current.uvi;
       let name = city;
  
-
+      // adds to HTML with proper symbols and lettering 
       document.getElementById('icon').setAttribute('src', icon);
       document.getElementById("cityDescription").innerHTML = description;
       document.getElementById("cityTemp").innerHTML = temp + "°";
@@ -57,7 +64,7 @@ function handleWeather() {
       document.getElementById("cityUVI").innerHTML = uvi;
       document.getElementById("cityName").innerHTML = name;
       document.getElementById("currentDate").innerHTML = moment().format("MMM Do YYYY");
-
+      // color change statment for uvi color change
         $("#cityUVI").each(function () {
          if (uvi < 3) {
           $(this).addClass("favorable");
@@ -76,16 +83,16 @@ function handleWeather() {
       }
          });
     
-
+      // clears 5 day forecast so it does not repeat the previous cities 5 day forcast searched 
       document.getElementById('forecast').innerHTML = '';
-
+      // loops weather info over 5 day forecast 
       for (let i = 0; i < 5; i++) {
         let date = moment().add(i + 1, "days").format("MM/DD/YYYY");
         let icon2 = data.daily[i].weather[0].icon;
         let temp = data.daily[i].temp.day;
         let wind = data.daily[i].wind_speed;
         let humidity = data.daily[i].humidity;
-
+      // dynamically adds 5 day info to html 
         document.getElementById('forecast').innerHTML +=
           `<div class="forecastCard">
               <h5>${date}</h5>
@@ -96,19 +103,18 @@ function handleWeather() {
               </div>`
 
       }
-
-      document.querySelector(".hide").style.display = "block";
+          
     });
+    // hides info on main screen and displays upon search 
+    document.querySelector(".hide").style.display = "block";
   });
 };
 
-
-$(window).on("load", function () {
-  $("#city-input").prop("value", citiesArr[0]);
-  handleWeather();
-})
-
+// clears local storage, innerHtml, and reloads on click 
 function handleClear(){
-  document.getElementById('searchHistory').innerHTML = '';
   localStorage.clear();
+  document.getElementById('searchHistory').innerHTML = '';
+  window.location.reload();
+  
 }
+
